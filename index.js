26c6b672536b066007285c5274d2d1f3ca8a24a5
Ragab <mrajab2112@gmail.com>
2019-05-20 01:25:57 +0200

    Added integration with redux

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-/**
- * @format
- */
-
+import React from 'react'
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import configureStore from './src/store/configureStore'
+import {Provider} from "react-redux";
+
+const store = configureStore();
+
+const RNRedux = () => (
+    <Provider store={store}>
+        <App/>
+    </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => RNRedux);
